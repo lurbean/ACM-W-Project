d02f548d8267f42ee1bc31002ecb2bd2a464bb9b
Lavine <von.lavine@yahoo.com>
2018-10-27 10:31:17 -0400

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	public float rotationSpeed;
+	private CharacterController cc;
+
+	void Start()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		
+		Vector3 moveDirectionForward = transform.forward * verticalInput;
+
+		transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+
+		Vector3 direction = (moveDirectionForward).normalized;
+		Vector3 distance = direction * speed * Time.deltaTime;
+
+
+		cc.Move(distance);
+
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+
+		//print("Collisding with" + hit.collider.gameObject.name);
+
+	}
+	void OnCollisionEnter(Collision c)
+	{
+
+		//print("Player stopped colliding with" + c.collider.gameObject.name);
+
+
+	}
+
+}
